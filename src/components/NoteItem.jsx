312688c44935c56effd8f6dd09c92518bea78eb0
ifykenchi <ifykenchi@gmail.com>
2023-10-01 10:55:48 +0100

    added page functionalities

--- src/components/NoteItem.jsx
+++ src/components/NoteItem.jsx
@@ -3,7 +3,10 @@ import { Link } from 'react-router-dom'
 
 const NoteItem = ({note}) => {
   return (
-    <Link to={`/edit-note/${note.id}`}></Link>
+    <Link to={`/edit-note/${note.id}`} className='note'>
+        <h4>{note.title.length > 50 ? (note.title.substr(0, 50)) + '...' : note.title}</h4>
+        <p>{note.date}</p>
+    </Link>
   )
 }
 
--- src/pages/CreateNote.jsx
+++ src/pages/CreateNote.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import {Link} from 'react-router-dom'
+import {IoIosArrowBack} from 'react-icons/io'
+import {useState} from 'react';
 
 const CreateNote = () => {
+  const [title, setTitle] = useState('');
+  const [details, setDetails] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (title && details) {
+      console.log(title, details);
+    }
+  } 
+
   return (
-    <div>CreateNote</div>
+    <section>
+      <header className="create-note__header">
+        <Link to='/' className='btn'><IoIosArrowBack/></Link>
+        <button className='btn lg primary' onClick={handleSubmit}>Save</button>
+      </header>
+      <form className="create-note__form" onSubmit={handleSubmit}>
+        <input type="text" placeholder='Title' value={title} onChange={(e) => setTitle(e.target.value)} autoFocus />
+        <textarea rows="28" placeholder='Note details...' value={details} onChange={(e) => setDetails(e.target.value)}></textarea>
+      </form>
+    </section>
   )
 }
 
